ede97f6f1bb0437127eff1f01cda5798ff9c0b72
A.karak <106385729+karak02@users.noreply.github.com>
2022-12-27 10:04:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// $(".user input").on("focus",function(){
+//     $(this).addClass("focus");
+// });
+
+// $(".user input").on("focus",function(){
+//     if($(this).val()=="")
+//     $(this).addClass("focus");
+// });
